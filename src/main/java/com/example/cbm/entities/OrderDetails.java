55e7e267e25dd6a1e6a7494a0490fc6a3f5f5793
Adkarthik117 <karthikaddanki11@gmail.com>
2023-07-13 20:29:09 +0530

    Initial commit

---
+++ src/main/java/com/example/cbm/entities/OrderDetails.java
@@ -0,0 +1,30 @@
+package com.example.cbm.entities;
+import lombok.*;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+@Table(name="orderdetails")
+public class OrderDetails {
+    @EmbeddedId
+    private OrderDetailsId id;
+    @MapsId("orderNumber")
+    @ManyToOne
+    @JoinColumn(name = "orderNumber")
+    private Orders order;
+    @MapsId("productCode")
+    @ManyToOne
+    @JoinColumn(name = "productCode")
+    private Products product;
+    @Column
+    private Integer quantityOrdered;
+    @Column
+    private BigDecimal priceEach;
+    @Column
+    private Short orderLineNumber;
+}
